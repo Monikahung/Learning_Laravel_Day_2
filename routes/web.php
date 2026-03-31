4258f08ff3288d3f89b7cee21fe52a56ce065dd5
Monikahung <monikahung580@gmail.com>
2026-03-31 08:26:51 +0700

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -65,3 +65,33 @@ Route::get(
     '/listbarang', 
     [BljrController::class, 'listbarang']
 )->name('databarang'); // Menambahkan nama route untuk list barang
+
+// Membuat route /formhitung untuk menampilkan hasil dari function fhitung di controller BljrController (view formhitung di dalam folder admin berupa form penjumlahan)
+Route::get(
+    '/formhitung',
+    [BljrController::class, 'fhitung']
+)->name('formhitung');
+
+// Membuat route /proseshitung untuk memproses data dari formhitung dengan method POST dan menampilkan hasil penjumlahan dari angka1 dan angka2 yang diinputkan di formhitung, menggunakan function calculate di controller BljrController
+Route::post(
+    '/proseshitung',
+    [BljrController::class, 'calculate']
+)->name('proseshitung');
+
+// Membuat route /formregister untuk menampilkan hasil dari function formregister di controller BljrController (view formregister di dalam folder admin berupa form registrasi)
+Route::get(
+    '/formregister',
+    [BljrController::class, 'fregister']
+)->name('formregister');
+
+// Membuat route /prosesregister untuk memproses data dari formregister dengan method POST dan menampilkan hasil dari registrasi di formregister, menggunakan function daftar di controller BljrController
+Route::post(
+    '/prosesregister',
+    [BljrController::class, 'daftar']
+)->name('prosesregister');
+
+// Membuat route /listgempa untuk menampilkan hasil dari function listgempa di controller BljrController (view datagempa di dalam folder admin berupa table)
+Route::get(
+    '/listgempa',
+    [BljrController::class, 'listgempa']
+)->name(name: 'datagempa');
