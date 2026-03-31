67440b9e9dc8b54f3e7d5060fbd856da082aa235
Monikahung <monikahung580@gmail.com>
2026-03-31 08:17:44 +0700

    Add formregister.blade.php

---
+++ resources/views/admin/formregister.blade.php
@@ -0,0 +1,68 @@
+@extends('admin.layout')
+
+@section('content')
+  <div class="content-wrapper">
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">FORM REGISTER</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              {{-- Menampilkan pesan sukses jika pendaftaran berhasil dari function daftar (route /prosesregister) --}}
+              @if(session('success'))
+              <p style="color: green;">{{ session('success') }}</p>
+              @endif
+
+              {{-- Menampilkan eror ketika validasi dari function daftar (route /prosesregister) --}}
+              @if($errors->any())
+              <ul style="color:red;">
+                @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+              </ul>
+              @endif
+
+              {{-- Form untuk mengirimkan data (post) ke route prosesregister --}}
+              <form action="{{ route('prosesregister') }}" method="post">
+                <div class="card-body">
+                  @csrf {{-- Token keamanan sistem --}}
+                  {{-- Menginput nama --}}
+                  <div class="form-group">
+                    <label>Nama</label>
+                    <input type="text" class="form-control" name="nama" placeholder="Input Nama">
+                  </div>
+                  {{-- Menginput email --}}
+                  <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" class="form-control" name="email" placeholder="Input Email">
+                  </div>
+                  {{-- Menginput password --}}
+                  <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" name="password" placeholder="Input Password" required>
+                  </div>
+                  {{-- Menginput konfirmasi password --}}
+                  <div class="form-group">
+                    <label for="password_confirmation">Konfirmasi Password</label>
+                    <input type="password" class="form-control" name="password_confirmation" placeholder="Konfirmasi Password" required>
+                  </div>
+                </div>
+
+                {{-- Tombol Daftar --}}
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Daftar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+@endsection
